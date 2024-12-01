decb4d7283f4dce442f44356f498142963c85d02
CARLOS MIGUEL PAREJA <PCN0587@dlsud.edu.ph>
2024-12-01 22:19:31 +0800

    Overall Report

--- reports.php
+++ reports.php
@@ -425,6 +425,27 @@ document.getElementById('reportsBtn').addEventListener('click', function () {
 
     </div>
 
+
+<div class="flex-1 flex flex-col items-center justify-center ml-64">
+<div class="w-full max-w-md bg-white p-8 rounded-lg shadow-md mt-2">
+<h2 class="text-2xl font-bold mb-6 text-center">Overall Reports</h2>
+
+    <?php if (isset($_SESSION['error'])): ?>
+        <div class="mb-4 p-4 text-red-700 bg-red-100 rounded-lg">
+                <?php echo htmlspecialchars($_SESSION['error']);
+                unset($_SESSION['error']); ?>
+         </div>
+    <?php endif; ?>
+
+<form action="overall_report.php" method="POST" class="space-y-4">
+  
+    
+    <div class="flex justify-center items-center mt-6">
+        <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+            Generate Report
+        </button>
+    </div>
+</form>
 </body>
 
 
